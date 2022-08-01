ba72cd7d1c88b755663b9f64fbadcc90ef6ec36b
StevenJV <svore@navexglobal.com>
2022-08-01 11:17:31 -0400

    adds basic `list` and `add` endpoints

--- Program.cs
+++ Program.cs
@@ -13,7 +13,18 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "API for ToDo items.");
+
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
 
 app.Run();
 
