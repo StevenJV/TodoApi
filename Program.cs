a17055ecad15f6384af83d3cdc8ef6290629169d
StevenJV <svore@navexglobal.com>
2022-08-01 10:39:07 -0400

    adds EFCore in-memory db

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
@@ -12,3 +16,18 @@ app.UseHttpsRedirection();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
