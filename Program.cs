1714e028c25535119b499fe43430313d75a27d4a
StevenJV <svore@navexglobal.com>
2022-08-02 10:59:53 -0400

    uses DTO with subset of object

--- Program.cs
+++ Program.cs
@@ -16,33 +16,39 @@ app.UseHttpsRedirection();
 app.MapGet("/", () => "API for ToDo items.");
 
 app.MapGet("/todoitems", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
+    await db.Todos.Select(x => new TodoItemDTO(x)).ToListAsync());
 
 app.MapGet("/todoitems/complete", async (TodoDb db) =>
-    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+    await db.Todos.Select(x => new TodoItemDTO(x)).ToListAsync());
 
 app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
         is Todo todo
-            ? Results.Ok(todo)
+            ? Results.Ok(new TodoItemDTO(todo))
             : Results.NotFound());
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+app.MapPost("/todoitems", async (TodoItemDTO todoItemDTO, TodoDb db) =>
 {
-  db.Todos.Add(todo);
+  var todoItem = new Todo
+  {
+    IsComplete = todoItemDTO.IsComplete,
+    Name = todoItemDTO.Name
+  };
+
+  db.Todos.Add(todoItem);
   await db.SaveChangesAsync();
 
-  return Results.Created($"/todoitems/{todo.Id}", todo);
+  return Results.Created($"/todoitems/{todoItem.Id}", new TodoItemDTO(todoItem));
 });
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+app.MapPut("/todoitems/{id}", async (int id, TodoItemDTO todoItemDTO, TodoDb db) =>
 {
   var todo = await db.Todos.FindAsync(id);
 
   if (todo is null) return Results.NotFound();
 
-  todo.Name = inputTodo.Name;
-  todo.IsComplete = inputTodo.IsComplete;
+  todo.Name = todoItemDTO.Name;
+  todo.IsComplete = todoItemDTO.IsComplete;
 
   await db.SaveChangesAsync();
 
@@ -55,7 +61,7 @@ app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
   {
     db.Todos.Remove(todo);
     await db.SaveChangesAsync();
-    return Results.Ok(todo);
+    return Results.Ok(new TodoItemDTO(todo));
   }
 
   return Results.NotFound();
@@ -68,6 +74,7 @@ class Todo
   public int Id { get; set; }
   public string? Name { get; set; }
   public bool IsComplete { get; set; }
+  public string? Secret { get; set; }
 }
 
 class TodoDb : DbContext
@@ -76,4 +83,15 @@ class TodoDb : DbContext
       : base(options) { }
 
   public DbSet<Todo> Todos => Set<Todo>();
+}
+
+public class TodoItemDTO
+{
+  public int Id { get; set; }
+  public string? Name { get; set; }
+  public bool IsComplete { get; set; }
+
+  public TodoItemDTO() { }
+  public TodoItemDTO(Todo todoItem) =>
+  (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
 }
